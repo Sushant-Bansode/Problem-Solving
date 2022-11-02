b95e6dd84d440649d64de6995af4056a48bc2132
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2022-11-02 23:50:48 +0530

    Time: 4 ms (62.80%), Space: 9.1 MB (72.66%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-       vector <int> ans; 
-      
-      for (int i = 0; i <= m-1; i++){  
-      ans.push_back(nums1[i]);
-      }  
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
+    {
+        for (int i = m; i < nums1.size(); i++)
+            nums1[i] = INT_MIN;
         
-      for (int i = 0; i <= n-1; i++){   
-      ans.push_back(nums2[i]);
-      }
-      
-      nums1 = ans; 
+        int pointer_one = 0;
+        int pointer_two = 0;
         
-           sort(nums1.begin(),nums1.end());
+        while (pointer_one < (m + n) && pointer_two < n) {
+            if ((nums1[pointer_one] > nums2[pointer_two]) || (pointer_one >= m && nums1[pointer_one] == INT_MIN)) {
+                for (int i = (m + n - 1); i > pointer_one; i--)
+                    nums1[i] = nums1[i - 1];
+                nums1[pointer_one] = nums2[pointer_two];
+                pointer_two++;
+            }
+            else {
+                pointer_one++;
+            }
+        }
     }
 };
