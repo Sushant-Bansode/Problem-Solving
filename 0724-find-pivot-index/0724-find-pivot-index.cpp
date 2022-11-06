7c7c9ae7707a707a7fe879ab39e5ec4a4ccdc933
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2022-11-06 16:46:47 +0530

    Time: 23 ms (96.04%), Space: 31.1 MB (86.70%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& arr) {
-   // Typical O(N) Possible Solution With Prefix Sum Types Approach in C++.
-    int sum = 0; // initialize sum of whole array
-    int leftsum = 0; // initialize leftsum
+    int sum = 0; 
+    int leftsum = 0; 
     int n = arr.size( );
-    // Find sum of the whole array 
+ 
     for (int i = 0; i < n; ++i)
         sum += arr[i];
  
     for (int i = 0; i < n; ++i) {
-        sum -= arr[i]; // sum is now right sum for index i
- 
+        sum -= arr[i]; 
         if (leftsum == sum)
             return i;
  
