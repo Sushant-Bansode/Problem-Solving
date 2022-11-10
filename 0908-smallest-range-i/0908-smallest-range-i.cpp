0b565d574b3d62517f16a32921ed0ac5fc65a4e8
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2022-11-10 23:03:05 +0530

    Time: 33 ms (60.47%), Space: 15.5 MB (56.11%) - LeetHub

--- 0908-smallest-range-i/0908-smallest-range-i.cpp
+++ 0908-smallest-range-i/0908-smallest-range-i.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int smallestRangeI(vector<int>& nums, int k) {
         int max_element = nums[0], min_element = nums[0];
-        for (int i = 0; i<nums.size( ); ++i){ 
-        max_element = max(max_element, nums[i]); 
-        min_element = min(min_element, nums[i]);
+        for (auto &x : nums){ 
+        max_element = max(max_element, x); 
+        min_element = min(min_element, x);
         }
         
         return max(0,(max_element - min_element - 2*k)); 
