c643f82e99eb2a84c309dd584fb01246109afd55
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2022-11-13 16:35:56 +0530

    Time: 115 ms (85.52%), Space: 46.5 MB (97.85%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+     sort(nums.begin(), nums.end());    
+     for(int i = 0; i<nums.size(); ++i){
+         if(i>0 && nums[i]== nums[i-1])
+             return true;
+     }
+    return false;    
+    }
+};
