e71b057786838525a028bec51c62c43870863509
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2022-11-19 11:47:33 +0530

    Time: 3 ms (45.14%), Space: 5.9 MB (87.81%) - LeetHub

--- 0504-base-7/0504-base-7.cpp
+++ 0504-base-7/0504-base-7.cpp
@@ -3,7 +3,7 @@ public:
      string convertToBase7(int num) {
         int ans =0;
         int j = 1;
-        while(num)
+        while(num!=0)
         { 
             ans += (num%7)*j;
             num /=7;
