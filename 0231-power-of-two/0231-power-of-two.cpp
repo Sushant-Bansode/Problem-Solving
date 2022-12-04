4c79e9e64166d67a944c469b04cdb7df9aac3352
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2022-12-04 19:06:06 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (27.40%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-       for(int i = 0; i<= log2(n); ++i){
-        if(1<<i == n){
-         return true;   
-        }     
-       }
-    return false;   
+    long long x = n;
+        if(x == 0)
+     return false;
+     else   
+     return !(x&(x-1));  
     }
 };
