5f381ec74ec47143d3ddba87af3b421d46a9bc8d
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2022-12-17 07:47:00 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (11.26%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -4,10 +4,10 @@ public:
     int titleToNumber(string s) {   
       int n = s.length( );   
       int ans = 0;
-       int p = n-1; 
-        for(int i = 0; i < n; i++){
+       int p = 0; 
+        for(int i = n-1; i>=0; i--){
             int ch = s[i] - 64;
-          ans = ans + ch*pow(26,p--);    
+          ans = ans + ch*pow(26,p++);    
         }
 return ans;    
     }
