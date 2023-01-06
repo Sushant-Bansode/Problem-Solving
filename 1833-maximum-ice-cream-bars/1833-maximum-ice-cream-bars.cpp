f643392aac8d3d09f3e4138bc6d7f85cd75e1c33
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2023-01-06 22:25:29 +0530

    Time: 598 ms (5.01%), Space: 76.5 MB (32.82%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+   sort(costs.begin( ), costs.end( ));
+   int ans = 0;
+ long long sum = 0;
+        for(int i = 0; i< costs.size( ); ++i){
+            sum += costs[i];
+            if(sum <= coins){
+              ans++;  
+            }
+        }
+     return ans;   
+    }
+};
