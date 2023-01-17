5d8c116d66ae47ff53bafc16542a135823e22cc8
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2023-01-17 17:35:27 +0530

    Time: 134 ms (89.63%), Space: 57.3 MB (75.54%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
+        // Can Use Merge Sort Here.
         sort(nums.begin( ), nums.end( ));
         return nums;
     }
