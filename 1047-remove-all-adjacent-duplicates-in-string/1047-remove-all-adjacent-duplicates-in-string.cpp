d4de238f44d75214c26bd803cfc2003580a0fc72
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2023-01-23 13:48:37 +0530

    Time: 1310 ms (5.02%), Space: 780.4 MB (5.10%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -3,10 +3,10 @@ public:
     string removeDuplicates(string s) {
       stack<char> st;
     for(int i=0; i< s.length( ); ++i){
-     if(st.empty( ) || st.top( )!= s[i]){
-     st.push(s[i]);}    
+    if(!st.empty( ) && st.top( )== s[i]){
+     st.pop( );}    
      else{
-    st.pop( );  }
+    st.push(s[i]);  }
    }
         
  string ans="";
