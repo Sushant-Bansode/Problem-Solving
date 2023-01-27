690e340ddbb97d9edee080d224cf42f38b6fdfb4
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2023-01-27 23:51:15 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (91.65%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.cpp
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -2,8 +2,13 @@ class Solution {
 public:
     int hammingDistance(int x, int y) {
        int temp = x^y;
-  int ct1 = __builtin_popcount(temp);
- return ct1;       
+  int ct1 = 0;       
+ for(int i = 31; i>=0; i--){
+     if((temp &(1 <<i)) != 0){
+         ct1++;
+     }
+ }
+ return abs(ct1 );       
     }
 };
 
