d46a97af7776bd45bd0fa45345b3aa86faf3ef1f
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2023-01-30 13:53:22 +0530

    Time: 3 ms (38.83%), Space: 6 MB (37.24%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        long long arr[n+10];
+    arr[0] = 0;
+    arr[1] = 1; 
+    arr[2] = 1;
+    for(int i = 3; i<= n; ++i){
+     arr[i] = arr[i-1] + arr[i-2] + arr[i-3];   
+    }
+return arr[n];        
+    }
+};
