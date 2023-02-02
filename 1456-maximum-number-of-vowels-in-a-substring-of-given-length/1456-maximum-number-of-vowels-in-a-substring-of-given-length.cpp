9a6169fe071166c176bf8c8c61200e4fe1a0f493
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2023-02-02 08:32:22 +0530

    Time: 19 ms (87.80%), Space: 10 MB (30.37%) - LeetHub

---
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+ static bool isvowel(char c){
+     if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+         return true;
+     }
+ return false;    
+ }   
+    int maxVowels(string s, int k) {
+       int start = 0;
+// Initialising with No of Bs in the 1st Window of size K.
+for(int i =0; i<k ;i++){
+  if(isvowel(s[i]))
+    start++;
+}
+// Typical Sliding Window For the Sake of the maximum of vowels in a Window of Size K.        
+// window variable
+int temp = start;
+for(int i =0, j = k; j< s.length( ); i++, j++ ){
+ // i it is the Starting index of the sliding window.
+ // j it is the ending index of the sliding window.
+ // if the Previous 1st starting index of the window was 'B' , then do temp-- or Else if the New upcoming to be added end index is, 'B' then add in temp.  
+            if(isvowel(s[i])){
+                temp--;  
+            }
+            if(isvowel(s[j])){
+                temp++;   
+            }
+// Simultaneously Keep Calculating the maximum count of the B as Every Window Proceeds via adding the next and subtracting the lasr one.            
+            start = max(start,temp);
+    }    
+        return start;                
+    }
+};
