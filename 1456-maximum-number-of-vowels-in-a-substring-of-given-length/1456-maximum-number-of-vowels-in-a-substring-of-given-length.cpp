0137905c03fb11018324695d416f8e9986cce9f1
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2023-02-02 08:33:22 +0530

    Time: 21 ms (77.98%), Space: 9.9 MB (68.17%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -8,7 +8,7 @@ public:
  }   
     int maxVowels(string s, int k) {
        int start = 0;
-// Initialising with No of Bs in the 1st Window of size K.
+// Initialising with No of vowels in the 1st Window of size K.
 for(int i =0; i<k ;i++){
   if(isvowel(s[i]))
     start++;
@@ -19,14 +19,14 @@ int temp = start;
 for(int i =0, j = k; j< s.length( ); i++, j++ ){
  // i it is the Starting index of the sliding window.
  // j it is the ending index of the sliding window.
- // if the Previous 1st starting index of the window was 'B' , then do temp-- or Else if the New upcoming to be added end index is, 'B' then add in temp.  
+ // if the Previous 1st starting index of the window was vowel , then do temp-- or Else if the New upcoming to be added end index is, vowel then add in temp.  
             if(isvowel(s[i])){
                 temp--;  
             }
             if(isvowel(s[j])){
                 temp++;   
             }
-// Simultaneously Keep Calculating the maximum count of the B as Every Window Proceeds via adding the next and subtracting the lasr one.            
+// Simultaneously Keep Calculating the maximum count of the vowel as Every Window Proceeds via adding the next and subtracting the lasr one.            
             start = max(start,temp);
     }    
         return start;                
