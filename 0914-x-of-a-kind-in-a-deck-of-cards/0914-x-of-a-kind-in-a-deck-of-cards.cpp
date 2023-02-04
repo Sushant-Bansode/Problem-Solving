044774d431a56cd8b1aae617999b7954d28dd63c
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2023-02-04 12:46:56 +0530

    Time: 11 ms (96.55%), Space: 17.4 MB (92.98%) - LeetHub

--- 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.cpp
+++ 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.cpp
@@ -3,9 +3,7 @@ public:
 bool hasGroupsSizeX(vector<int>& deck) {
     map<int,int> mp;
     for(auto x: deck){mp[x]++;}
-   if(mp.size( ) == 1 && mp[deck[0]] <=1){
-       return false;
-   }     
+    
  vector<int> freq;
  for(auto x: mp){freq.push_back(x.second);}
  int gcd_val = freq[0];
