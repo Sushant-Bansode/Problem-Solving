5a08e1ecbe38162d1a0139506d00e4fc30ef0ffb
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2023-02-08 17:46:23 +0530

    Time: 4 ms (55.29%), Space: 10.4 MB (60.00%) - LeetHub

---
+++ 1037-valid-boomerang/1037-valid-boomerang.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isBoomerang(vector<vector<int>>& points) {
+     int x1 = points[0][0];
+     int x2 = points[1][0];
+     int x3 = points[2][0];
+     int y1 = points[0][1];
+     int y2 = points[1][1];
+     int y3 = points[2][1];
+     int k  = abs(x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2));
+     return (k != 0);   
+    }
+};
