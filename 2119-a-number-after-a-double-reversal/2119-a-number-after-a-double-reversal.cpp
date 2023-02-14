96a28498efb9d770ef9de06d6aa9cd7ebcc7684b
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2023-02-14 17:24:12 +0530

    Time: 3 ms (37.09%), Space: 6.1 MB (6.17%) - LeetHub

---
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isSameAfterReversals(int num) {
+        if(num == 0){
+            return true;
+        }
+        if(num%10 == 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+};
