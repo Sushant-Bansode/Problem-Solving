96be1fad684abfd5e13f3e4c3f83afba759993c6
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2023-02-20 20:32:21 +0530

    Time: 566 ms (29.75%), Space: 144.3 MB (41.71%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -9,7 +9,7 @@ public:
         for (int i = 0; i < n; i++) {
             mp[changed[i]]++;
         }
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++){
           if (mp[changed[i]] == 0) continue;
           if (mp[changed[i] * 2] == 0) return {};
           ans.push_back(changed[i]);
