bc5f6fc9d09b2099ad269e5382a084a21b47dfd5
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2023-03-01 23:35:22 +0530

    Time: 1417 ms (16.81%), Space: 191.6 MB (13.31%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& arr) {
-    sort(arr.begin( ), arr.end( ));
-      int n = arr.size( );  
-     vector<vector<int>> ans;
-    
-    for(int i = 0; i< n; ++i){
-        int target = -arr[i];
-        
-        int low = i+1, high = n -1;
-    while(low< high){
-            if(arr[low] + arr[high] == target){
-                ans.push_back({arr[i],arr[low],arr[high]});
-    while(low<high && arr[low] == arr[low+1]) low++;
-    while(low<high && arr[high] == arr[high-1]) high--;  
-                low++;
-                high--;
-            }else if(arr[low] + arr[high] > target){
-                high--;
-            }else{
-                low++;
+    vector<vector<int>> threeSum(vector<int>& nums){
+       int target = 0;
+        sort(nums.begin(), nums.end());
+        set<vector<int>> s;
+        vector<vector<int>> output;
+        for (int i = 0; i < nums.size(); i++){
+            int j = i + 1;
+            int k = nums.size() - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target) {
+                    s.insert({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                } else if (sum < target) {
+                    j++;
+                } else {
+                    k--;
+                }
             }
-                 }
-        while(i+1 < n && arr[i+1] == arr[i]) i++;
-       }
-   return ans;     
+        }
+        for(auto triplets : s)
+            output.push_back(triplets);
+        return output;
     }
 };
