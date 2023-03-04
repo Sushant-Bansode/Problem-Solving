614255377c0a4ba1ff5e19c09597937a03e09d6a
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2023-03-04 15:00:12 +0530

    Time: 49 ms (99.94%), Space: 27.5 MB (82.49%) - LeetHub

---
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
@@ -0,0 +1,14 @@
+int k1;
+static bool comp(vector<int>&a,vector<int>&b) {
+      return a[k1]>b[k1];  
+}
+
+class Solution {
+public:   
+vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
+ k1=k;    
+ sort(score.begin(),score.end(),comp);
+  return score;
+        
+    }
+};
