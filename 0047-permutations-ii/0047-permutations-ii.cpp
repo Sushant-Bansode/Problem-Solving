49164a2c1450542534a830c8b674d5b79a352de4
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2023-03-13 19:34:21 +0530

    Time: 3 ms (96.87%), Space: 8.5 MB (81.70%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& arr) {
+         vector<int> temp = arr;      
+     vector<vector<int>> ans;
+      ans.push_back(temp);
+        while(1){
+          next_permutation(temp.begin(),temp.end());   
+          if(temp == arr){
+             break;   
+          }else{
+             ans.push_back(temp);  
+          } 
+        }
+ return ans;   
+    }
+};
