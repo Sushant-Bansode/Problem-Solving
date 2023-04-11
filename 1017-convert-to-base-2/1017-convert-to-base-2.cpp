f82c8d826c78da783e13b1b4fe820db5c2d35fe1
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2023-04-11 18:17:35 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (87.91%) - LeetHub

---
+++ 1017-convert-to-base-2/1017-convert-to-base-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+string baseNeg2(int N){
+    string res = "";
+      while (N != 0) {
+    int rem = N % -2;
+    N /= -2;
+    if (rem < 0) rem += 2, N += 1;
+    res = to_string(rem) + res;
+  }
+  return max(string("0"), res);
+    }
+};
