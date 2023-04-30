d4ea1adc5715164ab124324f44286bbbeb27e57b
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2023-04-30 11:00:56 +0530

    Time: 2 ms (69.28%), Space: 5.9 MB (58.45%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,28 +1,8 @@
 class Solution {
 public:
-
-    int square_root(int n)
-    {
-        if (n == 1 || n == 0)
-            return n;
-        int start = 0;
-        int end = n;
-        while (start <= end)
-        {
-            long int mid = start + (end - start) / 2;
-            if (mid * mid == n)
-                return mid;
-            else if (mid * mid < n)
-                start = mid + 1;
-            else if (mid * mid > n)
-                end = mid - 1;
-        }
-        return end;
-    }
-
     bool judgeSquareSum(int n) {
     long int start = 0;
-    long int end = square_root(n);
+    long int end = sqrt(n);
     while (start <= end)
     {
         long int sum = start * start + end * end;
