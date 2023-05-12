dd188d6d79b0a46b76efe1b358732abb037b11f0
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2023-05-12 19:33:03 +0530

    Time: 8 ms (72.17%), Space: 11.8 MB (75.91%) - LeetHub

--- 0089-gray-code/0089-gray-code.cpp
+++ 0089-gray-code/0089-gray-code.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> grayCode(int n) {
         vector<int> v;
-        int p=(1<<n);
+        int p = pow(2,n);
         for(int i=0; i<p; i++){
             v.push_back(i^(i/2));
         }
