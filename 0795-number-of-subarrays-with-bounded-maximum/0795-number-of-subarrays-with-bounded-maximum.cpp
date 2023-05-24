467af19575458fbaa35135ebe7e36d1c8bfd05f4
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2023-05-25 01:56:07 +0530

    Time: 71 ms (75.66%), Space: 52.4 MB (26.10%) - LeetHub

---
+++ 0795-number-of-subarrays-with-bounded-maximum/0795-number-of-subarrays-with-bounded-maximum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
+        return count(nums, right) - count(nums, left - 1);
+    }
+    int count(const vector<int>& nums, int bound) {
+        int ans = 0, cnt = 0;
+        for (int x : nums) {
+            cnt = x <= bound ? cnt + 1 : 0;
+            ans += cnt;
+        }
+        return ans;
+    }
+
+};
