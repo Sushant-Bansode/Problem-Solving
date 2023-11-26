f847f1ebd164844f61c541bf4e50a27e62745363
Sushant Bansode <97442721+Sushant-Bansode@users.noreply.github.com>
2023-11-26 22:24:46 +0530

    Time: 1575 ms (22.47%), Space: 0B (100.00%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div184-department-highest-salary/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div184-department-highest-salary.sql
@@ -0,0 +1,10 @@
+/* Write your PL/SQL query statement below */
+SELECT DEPT.name AS Department, EMP.name AS Employee, EMP.salary AS 
+
+Salary FROM Department DEPT, Employee EMP WHERE
+
+EMP.departmentId = DEPT.id AND (EMP.departmentId, salary) IN 
+
+(SELECT departmentId, MAX (salary) FROM Employee GROUP BY 
+
+departmentId)
